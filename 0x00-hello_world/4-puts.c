28cfef4ba77e1755757787af80bcdf9497e54251
W Hughes <willskhughes.edu@gmail.com>
2024-06-20 12:32:26 -0700

    Add: 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: This function prints a line of string using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
