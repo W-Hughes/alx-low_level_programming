96a11338e48cef2f506181b6a858d0afd4f27942
W Hughes <willskhughes.edu@gmail.com>
2024-06-25 22:58:23 -0700

    Add: files 9-times_table & 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - find the sum of two integers
+ *
+ * @a: first addened
+ * @b: second addened
+ *
+ *Return: the sum ob the integer.
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
