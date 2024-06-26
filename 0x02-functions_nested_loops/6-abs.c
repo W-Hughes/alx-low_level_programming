409670e4771efd19a4c8a408d568249f2f55105c
W Hughes <willskhughes.edu@gmail.com>
2024-06-25 21:35:20 -0700

    Add 6-abs file that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_abs - computes the absolute value of an integer values
+ *@n: The integer to comptute the absulute value of
+ *
+ *Return: the absolute value of the integer
+ */
+
+int _abs(int n)
+{
+	return ((n >= 0) ? n : -n);
+}
