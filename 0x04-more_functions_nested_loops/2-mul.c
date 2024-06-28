9c8b67bc6eb4b8b7a94d8fec42cd4b255ae7b42d
W Hughes <willskhughes.edu@gmail.com>
2024-06-28 08:37:00 -0700

    Add: 2-mul.c file that returns the product of two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - Multiplies two integers.
+ *@a: an integer that's a factor.
+ *@b: an integer to be multiplied(a factor).
+ *
+ * Return: the product of the factors.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
