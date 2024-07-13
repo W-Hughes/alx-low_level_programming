f3631cc0fbb3dd7338add8f26e099faae5a0ef73
W Hughes <willskhughes.edu@gmail.com>
2024-07-12 22:36:04 -0700

    Add: 0-whatismyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point of the code
+ *@argc: argument count
+ *@argv: String arguments at function call.
+ *
+ * Description: Function that prints its name, followed by new line
+ * Return: Always 0 (success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[00]);
+	return (0);
+}
