554ddd83a674963d45a9113ca87b9e0ea2216c0a
W-Hughes <willskhughes.edu@gmail.com>
2024-08-01 21:06:15 -0700

    Add: 1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - REturns the number of elements in a linked list.
+ *
+ * Return: Number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t node_counter;
+
+	node_counter = 0;
+	while (h)
+	{
+		h = h->next;
+		node_counter++;
+	}
+	return (node_counter);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +24,7 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
 
 #endif /* LISTS_H */
