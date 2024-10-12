f2f2f7029285f1f32ade000f2b572ef1113fdf00
H William <willskhughes.edu@gmail.com>
2024-10-11 23:00:49 -0700

    Add: 6-hash_table_delete.c file.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 void hash_table_print(const hash_table_t *ht);
-
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
